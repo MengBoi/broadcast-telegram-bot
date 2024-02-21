4f753c7a882a59942f10183824422eeef422a939
Earmeng Aing <earmeng.aing@prudential.com.kh>
2024-02-21 09:42:40 +0700

    done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 # Import the send_message function from your API module
 # from sendVideo import sendVideo
 from sendPhoto import sendPhoto
-filePath = "uat.txt"
+filePath = "botmember.txt"
 
 with open(filePath, "r") as file:
     lines = file.readlines()
